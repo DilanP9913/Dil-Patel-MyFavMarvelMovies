d6713fd8e6fb31748391701c89c1978f823ec956
DilanP9913 <105453257+DilanP9913@users.noreply.github.com>
2023-01-26 13:29:29 -0500

    Add files via upload

---
+++ src/app/content-list/content-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {Content} from '../helper-files/content-interface';
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
+})
+export class ContentListComponent implements OnInit {
+  mymarmovie:Content[];
+
+  constructor() {
+    this.mymarmovie = [{
+      id: 0,
+      title: 'IronMan',
+      description: "Armour Power",
+      creator: "Stan lee",
+      imgURL: "https://www.marvel.com/comics/issue/101529/iron_man_2022_1",
+      type: "action",
+      tags: ['action', "A.I", "robots"]
+    },{
+      id: 1,
+      title: 'Dr. Strange',
+      description: "Magic",
+      creator: "Stan Lee",
+      imgURL: "https://ultimate-marvel-cinematic-universe.fandom.com/wiki/Doctor_Strange",
+      type: "Sci-fi",
+      tags: ["Sci-Fi", "magic", "action"]
+    },{
+      id: 2,
+      title: 'Spiderman',
+      description: "Spider powersuit",
+      creator: "Stan Lee",
+      imgURL: "https://www.marvel.com/characters/spider-man-peter-parker",
+      type: "Comedy",
+      tags: ["action", "Sci-fi", "fight"]
+    }
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+}
