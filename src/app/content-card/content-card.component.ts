a9159fa8f7f814cfa8aaf2cb44f7a1585fc7697c
DilanP9913 <105453257+DilanP9913@users.noreply.github.com>
2023-01-26 13:30:17 -0500

    final changes done

---
+++ src/app/content-card/content-card.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Content} from "../helper-files/content-interface";
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit{
+
+  @Input() contentItem?: Content;
+  static contentCount = 0;
+  private _items: Content[];
+
+  constructor() {
+
+    this._items = []; // initialize arra
+  }
+
+  get items():Content[] {
+    return this._items;
+  }
+  myfun(){
+    console.log(this.contentItem?.id);
+    console.log(this.contentItem?.title);
+
+  }
+  ngOnInit(): void {
+  }
+}
