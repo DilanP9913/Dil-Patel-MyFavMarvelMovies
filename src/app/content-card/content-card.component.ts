a97497c4d89a83ed19438792a53a8c3afaaa7ee1
DilanP9913 <105453257+DilanP9913@users.noreply.github.com>
2023-02-02 22:37:03 -0500

    Add files via upload

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Content} from "../helper-files/content-interface";
 
 @Component({
-  selector: 'app-content-card',
+  selector: 'app-root',
   template: '<app-content-card></app-content-card>',
 })
 export class AppComponent {
