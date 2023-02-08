07ec7dbbad5101049cc20962c59488ee342c3d7e
Dilan Patel <w0775508@myscc.ca>
2023-02-08 13:32:04 -0500

    Content list card updated

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,10 +1,32 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Content} from "../helper-files/content-interface";
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
-  selector: 'app-root',
-  template: '<app-content-card></app-content-card>',
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
 })
-export class AppComponent {
+export class ContentCardComponent implements OnInit {
+ 
+  @Input() content?: Content;
+  private _item: Content[];
+  constructor() {
+    this._item=[];
+  }
 
+  getItem(): Content[] {
+    return this._item;
+  }
+
+  pritnItem() {
+    console.log(this.content?.id);
+    console.log(this.content?.title);
+    console.log(this.content?.description);
+    console.log(this.content?.creator);
+  }
+
+
+  ngOnInit(): void {
+  }
+  
 }
