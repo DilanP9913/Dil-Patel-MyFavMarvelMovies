c584ae895f53457fd0bf5942419d97786a4a0895
Dilan Patel <w0775508@myscc.ca>
2023-04-06 01:00:18 -0400

    in content-list.component.html changes done

--- src/app/marvel-movies.service.ts
+++ src/app/marvel-movies.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { content } from './helper-files/contenDb';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Content } from './helper-files/content-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarvelMoviesService {
+  private contentUrl = 'api/content';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor(private messages: MessageService) { }
+  constructor(private messages: MessageService, private http: HttpClient) { }
+  private log(message: string) {
+    console.log(`MarvelMoviesService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
 
   getContentArray(): Observable<any[]> {
     this.messages.add('Content array loaded!');
@@ -19,5 +36,23 @@ export class MarvelMoviesService {
     const x = content.find(item => item.id === id);
     this.messages.add(`Content Item at id: ${x?.id}`);
     return of(x);
+  }
+  addContent({ title, description }: { title: string; description: string; }): Observable<Content> {
+    const newContent: Content = {
+      //id: null,
+      title: title,
+      description: description,
+    };
+    return this.http.post<Content>(this.contentUrl, newContent, this.httpOptions).pipe(
+      tap((content: Content) => this.log(`added content with id ${content.id}`)),
+      catchError(this.handleError<Content>('addContent'))
+    );
+  }
+
+  updateContent(content: any): Observable<any> {
+    const index = content.findIndex(content.id === content.id);
+    content[index] = content;
+    this.messages.add(`Content item with id ${content.id} updated!`);
+    return of(content);
   }
 }
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,9 +1,82 @@
 import { Injectable } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import {InMemoryDbService} from 'angular-in-memory-web-api';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDataService{
 
   constructor() { }
+
+  createDb() {
+    const content : Content[] = [
+      {
+          id: 0,
+          title: 'IronMan',
+          description: "Armour Power",
+          creator: "Stan lee",
+          imgURL: "https://www.marvel.com/comics/issue/101529/iron_man_2022_1",
+          type: "action",
+          tags: ['action', "A.I", "robots"]
+        },{
+          id: 1,
+          title: 'Dr. Strange',
+          description: "Magic",
+          creator: "Stan Lee",
+          imgURL: "https://ultimate-marvel-cinematic-universe.fandom.com/wiki/Doctor_Strange",
+          type: "Sci-fi | drama",
+          tags: ["Sci-Fi", "magic", "action"]
+        },{
+          id: 2,
+          title: 'Spiderman',
+          description: "Spider powersuit",
+          creator: "Stan Lee",
+          imgURL: "https://www.marvel.com/characters/spider-man-peter-parker",
+          //type: "Comedy",
+          tags: ["action", "Sci-fi", "fight"]
+        }
+        ,{
+          id: 3,
+          title: 'Spiderman',
+          description: "Spider powersuit",
+          creator: "Stan Lee",
+          imgURL: "https://www.marvel.com/characters/spider-man-peter-parker",
+          //type: "Comedy",
+          tags: ["action", "Sci-fi", "fight"]
+        }
+        ,{
+          id: 4,
+          title: 'Spiderman',
+          description: "Spider powersuit",
+          creator: "Stan Lee",
+          imgURL: "https://www.marvel.com/characters/spider-man-peter-parker",
+          type: "Comedy",
+          tags: ["action", "Sci-fi", "fight"]
+        }
+        ,{
+          id: 5,
+          title: 'Spiderman',
+          description: "Spider powersuit",
+          creator: "Stan Lee",
+          imgURL: "https://www.marvel.com/characters/spider-man-peter-parker",
+          type: "Comedy | drama",
+          tags: ["action", "Sci-fi", "fight"]
+        }
+        ,{
+          id: 6,
+          title: 'Spiderman',
+          description: "Spider powersuit",
+          creator: "Stan Lee",
+          imgURL: "https://www.marvel.com/characters/spider-man-peter-parker",
+          type: "Comedy",
+          tags: ["action", "Sci-fi", "fight"]
+        }
+    ];
+    return {content};
+  }
+  etContentById(content: Content[]): number {
+    const maxId = content.length > 0 ? Math.max(...content.map(content => content.id || 0)) : 0;
+    return maxId + 1;
+  }
 }
