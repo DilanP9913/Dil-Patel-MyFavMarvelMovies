8bb912ce0653be5cd06c017f29db6cf1911c834c
Dilan Patel <w0775508@myscc.ca>
2023-04-06 16:53:46 -0400

    added matbuttonmodule and matinputmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { HttpClient } from '@angular/common/http';
 import { MarvelMoviesService } from './marvel-movies.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -30,7 +32,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     FormsModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [ MarvelMoviesService],
   bootstrap: [AppComponent]
