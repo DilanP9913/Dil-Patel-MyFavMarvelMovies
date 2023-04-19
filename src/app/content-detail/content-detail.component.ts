7c371f462a74a4fba12b2a352573e240c62aa645
Dilan Patel <w0775508@myscc.ca>
2023-04-19 19:54:19 -0400

    added component content-detail

---
+++ src/app/content-detail/content-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ContentService } from '../content.service';
+import { MessageService } from '../message.service';
+
+
+@Component({
+  selector: 'app-content-detail',
+  templateUrl: './content-detail.component.html',
+  styleUrls: ['./content-detail.component.scss']
+})
+export class ContentDetailComponent {
+
+}
